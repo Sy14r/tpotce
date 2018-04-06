b3adb6685f3c1d2002f49efcb5dfd7192e23e94c
Geoff Pamerleau <geoff@pamerleau.com>
2018-04-06 15:17:49 -0500

    adding sensor support

---
+++ docker/logstash-client/dist/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Let's ensure normal operation on exit or if interrupted ...
+function fuCLEANUP {
+  exit 0
+}
+trap fuCLEANUP EXIT
+
+# Download updated translation maps
+cd /etc/listbot 
+git pull
+cd /
+
+sed -i /etc/logstash/conf.d/logstash.conf -e 's/127.0.0.1/$SERVER_IP/g'
